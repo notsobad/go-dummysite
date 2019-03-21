036f2e24c049d45430fecb1dfc2c30853f6c4f66
notsobad <wangxiaohugg@gmail.com>
2019-03-22 00:12:05 +0800

    add mimetype

--- fakesite.go
+++ fakesite.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
+	"mime"
 	"net/http"
+	"path"
 	"strconv"
 	"time"
 )
@@ -15,15 +17,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+	filename := vars["filename"]
 	now := time.Now()
 	cacheTime := 95270
-
 	expired := now.Add(time.Second * time.Duration(cacheTime))
+
+	contentType := mime.TypeByExtension(path.Ext(filename))
 
 	w.Header().Set("Last-Modified", now.Format(time.RFC1123))
 	w.Header().Set("Expires", expired.Format(time.RFC1123))
 	w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%d", cacheTime))
-	fmt.Fprintf(w, vars["filename"])
+
+	if contentType != "" {
+		w.Header().Set("Content-Type", contentType)
+	}
+
+	fmt.Fprintf(w, filename)
 }
 
 func codeHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,12 +47,16 @@ func codeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Http %s</h1> <hr/>Generated at %s", vars["code"], now.Format(time.RFC3339))
 }
 
+func dynamicHandler(w http.ResponseWriter, r *http.Request) {
+}
+
 func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler)
 	r.HandleFunc("/static/{filename:.*}", staticHandler)
 	r.HandleFunc("/code/{code:[1-5][0-9][0-9]}", codeHandler)
+	r.HandleFunc("/dynamic/{filename:.*}", dynamicHandler)
 
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", r))
 }
